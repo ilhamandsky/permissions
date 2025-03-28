b30779d4928b565d752e9ac66a0d2346d5a98d21
M. Imam Firdaus <mimamfirdaus5@gmail.com>
2025-03-28 23:58:38 +0700

    fix

--- database/migrations/2025_03_27_000002_create_reservations_table.php
+++ database/migrations/2025_03_27_000002_create_reservations_table.php
@@ -4,28 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+return new class extends Migration {
+    public function up()
     {
         Schema::create('reservations', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('room_id')->constrained()->onDelete('cascade');
+            $table->enum('status', ['pending', 'confirmed', 'canceled'])->default('pending');
             $table->date('check_in');
-            $table->date('check_out')->nullable();
-            $table->enum('status', ['pending', 'accepted', 'rejected'])->default('pending');
+            $table->date('check_out');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('reservations');
     }
