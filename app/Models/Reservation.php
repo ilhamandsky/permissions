c19213d2d87c7b58d952b29f18261d28237bcfce
M. Imam Firdaus <mimamfirdaus5@gmail.com>
2025-03-28 23:46:19 +0700

    perbarui kode

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -1,9 +1,12 @@
 <?php
 
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Model;
+
 class Reservation extends Model {
-    protected $fillable = ['user_id', 'room_id', 'status'];
+    protected $fillable = ['user_id', 'room_id', 'status', 'check_in', 'check_out'];
+
     public function room() {
         return $this->belongsTo(Room::class);
     }
