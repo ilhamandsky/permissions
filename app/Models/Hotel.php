4e39402fc50b87784bacc21200d1d1f38bb14552
M. Imam Firdaus <mimamfirdaus5@gmail.com>
2025-03-28 22:03:16 +0700

    perbaikan kode

--- app/Models/Hotel.php
+++ app/Models/Hotel.php
@@ -1,11 +1,15 @@
 <?php
 
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+
 class Hotel extends Model {
     use HasFactory;
-    protected $fillable = ['name', 'description'];
+
+    protected $fillable = ['name', 'description', 'location']; // Tambahkan 'location'
+
     public function rooms() {
         return $this->hasMany(Room::class);
     }
